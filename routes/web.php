495e9299817a0a0f26ac65b044fd3ddbe965faf8
azzurragraziano <azzurragraziano02@email.com>
2022-08-24 16:04:18 +0200

    sass scaffolding

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('comics-page');
+});
+
+Route::get('/series', function () {
+    return view('series');
 });
